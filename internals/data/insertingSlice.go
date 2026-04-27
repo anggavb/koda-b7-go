8addd974b71e570499ea41d4ad79d8cfef11247d
anggavb <anggavb8@gmail.com>
2026-04-27 21:15:29 +0700

    feat(data): update InsertDataToSlice function to accept newNum parameter

--- internals/data/insertingSlice.go
+++ internals/data/insertingSlice.go
@@ -2,9 +2,8 @@ package data
 
 import "fmt"
 
-func InsertDataToSlice() {
+func InsertDataToSlice(newNum int) {
 	slice := []int{50,75,66,20,32,90}
-	newNum := 88
 
 	newSlice := make([]int, 0, 7)
 	newSlice = append(newSlice, slice[0:3]...)
