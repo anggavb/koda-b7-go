42069963d2db271e9b2a6c04fa82abbbf8c6c78b
anggavb <anggavb8@gmail.com>
2026-04-27 12:32:05 +0700

    feat(main): add triangle printing functionality and fix output formatting

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ func main() {
 	radius := 12
     area, circumference := calculateCircle(radius)
 
-	fmt.Printf("Jari-jari: %d\nLuas: %.2f\nKeliling: %.2f", radius, area, circumference)
+	fmt.Printf("Jari-jari: %d\nLuas: %.2f\nKeliling: %.2f\n", radius, area, circumference)
+	
+	input := 5
+	triangleFromTriangle(input)
 }
 
 func calculateCircle(r int) (area float32, circumference float32) {
@@ -28,4 +31,15 @@ func calculateCircleArea(r int, pi float32) (area float32) {
 func calculateCircleCircumference(r int, pi float32) (circumference float32) {
 	circumference = float32(2) * pi * float32(r)
 	return circumference
+}
+
+func triangleFromTriangle(input int) {
+	for i := range input {
+		var triangle string = "*"
+		for range i {
+			triangle += "*"
+		}
+
+		fmt.Println(triangle)
+	}
 }
