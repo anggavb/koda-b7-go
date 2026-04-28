d2b069471386fe6933cb0742887baf94194fcbf6
anggavb <anggavb8@gmail.com>
2026-04-28 11:38:21 +0700

    refactor(readfile): simplify ReadFile function by using io.ReadAll for file content

--- internals/readfile/readFile.go
+++ internals/readfile/readFile.go
@@ -2,6 +2,7 @@ package readfile
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -19,11 +20,29 @@ func ReadFile(filename string) (string, error) {
     defer file.Close()
 
 	// Read the file content
-	content := make([]byte, 1024)
-	n, err := file.Read(content)
+	content, err := io.ReadAll(file)
 	if err != nil {
 		panic(err.Error())
 	}
+	return string(content), nil
+
+	// using os.File
+	// content := make([]byte, 1024)
+	// n, err := file.Read(content)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
 	// convert byte slice to string cause content is ascii text of bytes
-	return string(content[:n]), nil
+	// return string(content[:n]), nil
+
+	// using bufio if the file is large, we can read it line by line
+	// scanner := bufio.NewScanner(file)
+	// var result string
+	// for scanner.Scan() {
+	// 	result += scanner.Text() + "\n"
+	// }
+	// if err := scanner.Err(); err != nil {
+	// 	panic(err.Error())
+	// }
+	// return result, nil
 }
