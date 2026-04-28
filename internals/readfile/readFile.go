23b0684976fac221002f837b5fc6ac702ca1b824
anggavb <anggavb8@gmail.com>
2026-04-28 11:21:30 +0700

    fix(readfile): improve error handling in ReadFile function

--- internals/readfile/readFile.go
+++ internals/readfile/readFile.go
@@ -1,11 +1,17 @@
 package readfile
 
 import (
+	"fmt"
 	"os"
 )
 
 // Minitask 6: Read File Content
 func ReadFile(filename string) (string, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered while reading file:", r)
+		}
+	}()
     file, err := os.Open(filename)
     if err != nil {
         return "", err
@@ -16,7 +22,8 @@ func ReadFile(filename string) (string, error) {
 	content := make([]byte, 1024)
 	n, err := file.Read(content)
 	if err != nil {
-		return "", err
+		panic(err.Error())
 	}
+	// convert byte slice to string cause content is ascii text of bytes
 	return string(content[:n]), nil
 }
